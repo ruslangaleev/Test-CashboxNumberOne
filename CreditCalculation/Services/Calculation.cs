f44d47020a9c3dfedba5fbbbf05bd3b6c0e06e00
ruslan.nafisovich@gmail.com <ruslan.nafisovich@gmail.com>
2018-07-09 22:23:35 +0300

    Добавлена документация

--- CreditCalculation/Services/Calculation.cs
+++ CreditCalculation/Services/Calculation.cs
@@ -1,8 +1,5 @@
 ﻿using CreditCalculation.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace CreditCalculation.Services
 {
@@ -11,14 +8,31 @@ namespace CreditCalculation.Services
         /// <summary>
         /// Рассчитает ежемесячную сумму кредита.
         /// </summary>
-        /// <param name="sum"></param>
-        /// <param name="termType"></param>
-        /// <param name="termValue"></param>
-        /// <param name="rateType"></param>
-        /// <param name="rateValue"></param>
-        /// <returns></returns>
+        /// <param name="sum">Сумма кредита.</param>
+        /// <param name="termType">Тип срока.</param>
+        /// <param name="termValue">Значение срока.</param>
+        /// <param name="rateType">Тип ставки.</param>
+        /// <param name="rateValue">Значение ставки.</param>
         public double Calculate(double sum, TermType termType, int termValue, RateType rateType, double rateValue)
         {
+            // TODO: Сделать рассчет срока по дням
+            // TODO: Сделать рассчет по ежедневным ставкам
+
+            if (sum < 0 )
+            {
+                throw new ArgumentException("Сумма кредита не должна быть менее 0.", nameof(sum));
+            }
+
+            if (termValue < 0)
+            {
+                throw new ArgumentException("Срок кредита не должен быть менее 0.", nameof(termValue));
+            }
+
+            if (rateValue < 0)
+            {
+                throw new ArgumentException("Процентная ставка не должна быть менее 0.", nameof(rateValue));
+            }
+
             var i = (rateValue / 12) / 100;
 
             var k = (i * Math.Pow((1 + i), termValue)) / (Math.Pow((1 + i), termValue) - 1);
--- CreditCalculationTests/CalculationTests.cs
+++ CreditCalculationTests/CalculationTests.cs
@@ -1,25 +1,24 @@
 ﻿using CreditCalculation.Models;
 using CreditCalculation.Services;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace CreditCalculationTests
 {
     [TestFixture]
     public class CalculationTests
     {
-        [Test]
-        public void Test()
-        {
-            // Подготовка.
-            var calculation = new Calculation();
+        Calculation _calculation;
 
+        [SetUp]
+        public void Setup()
+        {
+            _calculation = new Calculation();
+        }
+
+        [Test]
+        public void ReturnsSumMonthlyWithYearlyRate()
+        {
             // Действие.
-            var result = calculation.Calculate(30000, TermType.Month, 36, RateType.Year, 18);
+            var result = _calculation.Calculate(30000, TermType.Month, 36, RateType.Year, 18);
 
             // Проверка.
             Assert.AreEqual(1084.57, result);
